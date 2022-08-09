ccb668b6a29ed82462c1d66cd2a2ef0c7af0a7fb
Derek <dqdang17@gmail.com>
2022-08-09 01:14:59 -0700

    Add placeholder values

--- mesh/ContentView.swift
+++ mesh/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    let fontRegular = "JetBrainsMonoNL-Regular"
+    let fontBold = "JetBrainsMono-Bold"
+    let cryptoList = ["Bitcoin", "Ethereum", "Tether", "USD Coin", "BNB", "XRP", "Cardano", "Binance USD", "Solana", "Polkadot", "Dogecoin", "Avalanche", "Dai", "Polygon", "Shiba Inu", "Uniswap", "TRON", "Wrapped Bitcoin", "Ethereum Classic", "UNUS SED LEO", "Litecoin", "FTX Token", "NEAR Protocol", "Chainlink", "Cronos", "Cosmos", "Stellar", "Flow", "Monero", "Bitcoin Cash", "Algorand"]
+    let pricesList = ["23,797.81", "1,772.39", "1.00", "0.99", "324.27", "0.37", "0.53", "1.00", "42.42", "9.152", "0.07305", "28.38", "0.9994", "0.9232", "0.000001", "8.741", "0.0707", "23,853.45", "37.78", "4.778", "62.45", "31.44", "5.407", "8.596", "0.1520", "11.75", "0.1271", "3.022", "167.11", "143.24", "0.3659"]
+
     init() {
         UITabBar.appearance().backgroundColor = UIColor(Color("Background"))
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.init(name: fontRegular, size: 20)! ], for: .normal)
 
     }
-    let cryptoList = ["Bitcoin", "Ethereum", "Tether", "USD Coin", "BNB"]
+
     var body: some View {
         Color("Background").ignoresSafeArea(.all)
         .overlay(
@@ -35,28 +41,30 @@ struct ContentView: View {
                         Rectangle().frame(width: UIScreen.screenWidth - 50, height: UIScreen.screenHeight - 380, alignment: .center)
                         .foregroundColor(.gray)
                         .overlay(
-                        ScrollView {
+                            ScrollView(.vertical, showsIndicators: false) {
                             VStack (alignment: .leading) {
-                                Text("Cryptocurrencies")
-                                    .font(Font.custom("JetBrainsMono-Bold", size:20))
+                                let whiteSpaces = whiteSpaceGenerator(left: "Cryptocurrencies", right: "$", border: 26)
+                                Text("Cryptocurrencies \(whiteSpaces) $")
+                                    .font(Font.custom(fontBold, size:20))
                                     .frame(width: UIScreen.screenWidth - 77, alignment: .topLeading)
                                 ForEach(0 ..< cryptoList.count, id: \.self) {
-                                    Text(self.cryptoList[$0])
-                                        .font(Font.custom("JetBrainsMonoNL-Regular", size:15))
+                                    let whiteSpaces = whiteSpaceGenerator(left: cryptoList[$0], right: pricesList[$0], border: 36)
+                                    Text("\(cryptoList[$0]) \(whiteSpaces) \(pricesList[$0])")
+                                        .font(Font.custom(fontRegular, size:15))
                                         .frame(width: UIScreen.screenWidth - 77, alignment: .topLeading)
                                 }
                             }
                         }
                 ))))
                 TabView() {
-                    Text("").font(Font.custom("JetBrainsMonoNL-Regular", size: 10)).tabItem {
-                        Text("Crypto").font(Font.custom("JetBrainsMonoNL-Regular", size: 25))
+                    Text("").tabItem {
+                        Text("Crypto")
                     }
-                    Text("").font(Font.custom("JetBrainsMonoNL-Regular", size: 10)).tabItem {
-                        Text("Stocks").font(Font.custom("JetBrainsMonoNL-Regular", size: 25))
+                    Text("").tabItem {
+                        Text("Stocks")
                     }
-                    Text("").font(Font.custom("JetBrainsMonoNL-Regular", size: 10)).tabItem {
-                        Text("About").font(Font.custom("JetBrainsMonoNL-Regular", size: 25))
+                    Text("").tabItem {
+                        Text("About")
                     }
                 }.frame(width: UIScreen.screenWidth, height:40, alignment: .bottom)
                     .accentColor(Color("Selected"))
@@ -75,4 +83,15 @@ extension UIScreen{
    static let screenWidth = UIScreen.main.bounds.size.width
    static let screenHeight = UIScreen.main.bounds.size.height
    static let screenSize = UIScreen.main.bounds.size
+}
+
+func whiteSpaceGenerator(left: String, right: String, border: Int) -> String {
+    let leftStrLen = left.count
+    let righttStrLen = right.count
+    let numWhiteSpaces = border - leftStrLen - righttStrLen
+    var whiteSpaces = ""
+    for _ in 0...numWhiteSpaces {
+        whiteSpaces += " "
+    }
+    return whiteSpaces
 }
